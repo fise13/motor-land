814b6d4c2424efd699f65017d60bc44d5ef909bc
fise13 <victhewise@icloud.com>
2025-11-01 20:05:55 +0500

    update 2

--- motor-land.kz/hyst/visual/html/authorization.php
+++ motor-land.kz/hyst/visual/html/authorization.php
@@ -1,6 +1,6 @@
 <div class="auth_container">
   <div class="auth_form">
-    <div class="auth_form_logo"><img src="/hyst/visual/images/logo.png" alt="Моторленд"></div>
+    <div class="auth_form_logo"><img src="img/logo.jpg" alt="Моторленд"></div>
     <div class="form_title">Вход в систему</div>
     <interfaceform target="hyst/core/admin_profile">
       <input class="input_field" type="text" name="email" placeholder="Email" mandatory>
