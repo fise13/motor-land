2f59f9860cbc84fdddf284813c6e1c977c37123f
fise13 <victhewise@icloud.com>
2025-11-01 09:27:48 +0500

    new update

--- motor-land.kz/index.php
+++ motor-land.kz/index.php
@@ -16,7 +16,6 @@ $SITE_DESCRIPTION = 'Компания "Motor Land" - поставка контр
 <?php include("des/head.php"); ?>
 
 <div id="preloader">
-  <div class="loader-wrapper">
     <div class="loader-logo"></div>
   </div>
 </div>
