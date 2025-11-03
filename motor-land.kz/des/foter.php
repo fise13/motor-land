02dc77534f4515176e0999ef4ad70a83148a16c3
fise13 <victhewise@icloud.com>
2025-11-03 17:28:22 +0500

    Enhance footer form styles and structure for improved usability
    - Updated CSS for form controls, implementing flexbox for better alignment and layout.
    - Adjusted label positioning and animations for a more dynamic user experience.
    - Modified HTML structure in the footer to ensure consistent styling and improved visual feedback for input fields.

--- motor-land.kz/des/foter.php
+++ motor-land.kz/des/foter.php
@@ -2,21 +2,21 @@
 	<div class="shirina">
 	<div class="foterinline JF_parent_form">
   <div class="foterzago">Закажите обратный звонок</div>
-  <form method="post">
+  <form method="post" style="display: flex; flex-direction: column; align-items: center;">
     <div class="form-control">
-      <input type="text" name="name" id="name" required>
       <label for="name">
         <span style="transition-delay:0ms">И</span><span style="transition-delay:50ms">м</span><span style="transition-delay:100ms">я</span>
       </label>
+      <input type="text" name="name" id="name" required>
     </div>
     <div class="form-control">
-      <input type="text" name="phon" id="phon" required>
       <label for="phon">
         <span style="transition-delay:0ms">Т</span><span style="transition-delay:50ms">е</span><span style="transition-delay:100ms">л</span><span style="transition-delay:150ms">е</span><span style="transition-delay:200ms">ф</span><span style="transition-delay:250ms">о</span><span style="transition-delay:300ms">н</span>
       </label>
+      <input type="text" name="phon" id="phon" required>
     </div>
     <input type="hidden" name="send_one" value="send">
-    <input type="button" name="JF_send_casual" value="Отправить">
+    <input type="button" name="JF_send_casual" value="Отправить" style="margin-top: 20px;">
   </form>
 </div>
 
