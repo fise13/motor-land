5274f3ed7a4f2fde250beed3a1eff291e2b707ff
fise13 <victhewise@icloud.com>
2025-11-03 14:53:48 +0500

    Enhance contacts page layout and styling
    - Updated contacts.php to introduce a new card layout for contact information, improving readability and organization.
    - Enhanced CSS styles for better visual appeal, including animations and responsive design adjustments.
    - Added structured elements for address, phone numbers, and working hours to improve user experience.

--- motor-land.kz/contacts.php
+++ motor-land.kz/contacts.php
@@ -29,45 +29,69 @@ $SITE_DESCRIPTION = 'Контакты компании Motor Land - контра
 	</div>
 </div>
 
-<div class="generalw">
+<div class="generalw contacts-main">
 	<div class="shirina">
 		<div class="footer_contacts contacts-page">
-			<div class="footer_contact_block">
-				<div class="foterconicon" style="background-image: url(./img/loc.png);">
-					<a target="_blank" href="https://2gis.kz/almaty/search/РВ-90%2C%207-линия%2C%2029">РВ-90, 7-линия, 29</a>
+			<div class="footer_contact_block contact-card">
+				<div class="contact-card-header">
+					<h3 class="contact-office-title">Офис №1</h3>
 				</div>
-				<div class="foterconicon" style="background-image: url(./img/tel.png);">
-					<a href="tel:+77771445445" onclick="gtag('event', 'conversion', {'send_to': 'AW-17661940869/8IrgCNzqw7QbEIWp7-VB'});">+7 777 144 5445</a>
+				<div class="contact-card-content">
+					<div class="foterconicon contact-item" style="background-image: url(./img/loc.png);">
+						<span class="contact-label">Адрес:</span>
+						<a target="_blank" href="https://2gis.kz/almaty/search/РВ-90%2C%207-линия%2C%2029" class="contact-link">РВ-90, 7-линия, 29</a>
+					</div>
+					<div class="foterconicon contact-item" style="background-image: url(./img/tel.png);">
+						<span class="contact-label">Телефон:</span>
+						<a href="tel:+77771445445" onclick="gtag('event', 'conversion', {'send_to': 'AW-17661940869/8IrgCNzqw7QbEIWp7-VB'});" class="contact-link">+7 777 144 5445</a>
+					</div>
+					<div class="foterconicon contact-item" style="background-image: url(./img/wats.svg);">
+						<span class="contact-label">WhatsApp:</span>
+						<a href="https://wa.me/77771445445" class="contact-link">Написать в WhatsApp</a>
+					</div>
+					<div class="foterconicon contact-item" style="background-image: url(./img/day.png);">
+						<span class="contact-label">Режим работы:</span>
+						<div class="contact-schedule">
+							пн-пт | 9:00-18:00<br>
+							суббота | 10:00-15:00<br>
+							воскресенье | выходной
+						</div>
+					</div>	
 				</div>
-				<div class="foterconicon" style="background-image: url(./img/wats.svg);">
-					<a href="https://wa.me/77771445445">WhatsApp</a>
-				</div>
-				<div class="foterconicon" style="background-image: url(./img/day.png);">
-					пн-пт | 9:00-18:00<br>
-					суббота | 10:00-15:00<br>
-					воскресенье | выходной
-				</div>	
 			</div>
-			<div class="footer_contact_block">
-				<div class="foterconicon" style="background-image: url(./img/loc.png);">
-					<a target="_blank" href="https://2gis.kz/almaty/search/улица%20Свердлова%2C%2038">улица Свердлова, 38</a>
+			<div class="footer_contact_block contact-card">
+				<div class="contact-card-header">
+					<h3 class="contact-office-title">Офис №2</h3>
 				</div>
-				<div class="foterconicon" style="background-image: url(./img/tel.png);">
-					<a href="tel:+77011445445" onclick="gtag('event', 'conversion', {'send_to': 'AW-17661940869/8IrgCNzqw7QbEIWp7-VB'});">+7 701 144 5445</a><br>
-					<a href="tel:+77071445445" onclick="gtag('event', 'conversion', {'send_to': 'AW-17661940869/8IrgCNzqw7QbEIWp7-VB'});">+7 707 144 5445</a>
-				</div>
-				<div class="foterconicon" style="background-image: url(./img/wats.svg);">
-					<a href="https://wa.me/77011445445">WhatsApp</a>
-				</div>
-				<div class="foterconicon" style="background-image: url(./img/day.png);">
-					пн-пт | 9:00-18:00<br>
-					суббота | 10:00-15:00<br>
-					воскресенье | выходной
+				<div class="contact-card-content">
+					<div class="foterconicon contact-item" style="background-image: url(./img/loc.png);">
+						<span class="contact-label">Адрес:</span>
+						<a target="_blank" href="https://2gis.kz/almaty/search/улица%20Свердлова%2C%2038" class="contact-link">улица Свердлова, 38</a>
+					</div>
+					<div class="foterconicon contact-item" style="background-image: url(./img/tel.png);">
+						<span class="contact-label">Телефоны:</span>
+						<div class="contact-phones">
+							<a href="tel:+77011445445" onclick="gtag('event', 'conversion', {'send_to': 'AW-17661940869/8IrgCNzqw7QbEIWp7-VB'});" class="contact-link">+7 701 144 5445</a><br>
+							<a href="tel:+77071445445" onclick="gtag('event', 'conversion', {'send_to': 'AW-17661940869/8IrgCNzqw7QbEIWp7-VB'});" class="contact-link">+7 707 144 5445</a>
+						</div>
+					</div>
+					<div class="foterconicon contact-item" style="background-image: url(./img/wats.svg);">
+						<span class="contact-label">WhatsApp:</span>
+						<a href="https://wa.me/77011445445" class="contact-link">Написать в WhatsApp</a>
+					</div>
+					<div class="foterconicon contact-item" style="background-image: url(./img/day.png);">
+						<span class="contact-label">Режим работы:</span>
+						<div class="contact-schedule">
+							пн-пт | 9:00-18:00<br>
+							суббота | 10:00-15:00<br>
+							воскресенье | выходной
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
 		
-		<div class="map">
+		<div class="map contacts-map">
 			<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A7f3534207526eaa3fc4288c55720ef5bf87c1fae35fecbd8953f8b3dab48de0e&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
 		</div>
 	</div>
