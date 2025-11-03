24162af7e6d369e3045ea71bd5edbfa7c7ac2a6f
fise13 <victhewise@icloud.com>
2025-11-03 10:48:01 +0500

    admin test

--- motor-land.kz/hyst/core/html.php
+++ motor-land.kz/hyst/core/html.php
@@ -55,7 +55,20 @@ function hyst_show_adm () {
 		echo $html;
 		} else {
 		$html .= '<input type="checkbox" id="sidebarbutton"'.((empty($_COOKIE['sidebarbutton'])||$_COOKIE['sidebarbutton']=='false')?'':' checked').'>';
-		$html .= '<adm_header><label for="sidebarbutton"><i></i></label><div class="admin_header_profile"><div class="admin_header_name">'.$_HYST_ADMIN[AUC_PREFIX.'_name'].'</div><a class="admin_header_exit" href="?exit_admin">Выход</a></div></adm_header>';
+		$html .= '<adm_header>
+			<label for="sidebarbutton"><i></i></label>
+			<div class="admin_header_profile">
+				<div class="admin_header_name">'.$_HYST_ADMIN[AUC_PREFIX.'_name'].'</div>
+				<a class="admin_header_exit" href="?exit_admin">
+					<button class="Btn">
+						<div class="sign">
+							<svg viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path></svg>
+						</div>
+						<div class="text">Logout</div>
+					</button>
+				</a>
+			</div>
+		</adm_header>';
 		$html .= '<adm_content>';
 		$html .= '<sidebar>
 			<a class="admin_sidebar_menu" style="background-image: url(/hyst/visual/images/index_icon.svg);" href="/adm"><span>Контрольная панель</span></a>';
